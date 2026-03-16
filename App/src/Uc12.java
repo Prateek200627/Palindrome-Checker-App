204096e12e1d4ed1959c18c074cf2e47b923d82c
Parteek200627 <parteekbasu2006@gmail.com>
2026-03-16 10:04:56 +0530

    usercase12

---
+++ App/src/Uc12.java
@@ -0,0 +1,54 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class Uc12 {
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        PalindromeStrategy strategy;
+
+        // Use StackStrategy for this example; could be swapped dynamically
+        strategy = new StackStrategy();
+
+        boolean isPalindrome = strategy.check(input);
+
+        System.out.println("Input: " + input);
+        System.out.println("Is Palindrome? " + isPalindrome);
+    }
+}
